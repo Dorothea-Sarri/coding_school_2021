97ad065251d113ba9e115da895e395d9cf0d58cd
Dorothea Sarri <dorotheasarri95@gmail.com>
2021-06-10 09:37:24 +0300

    commit new

---
+++ WindowsFormsApp4/WindowsFormsApp4/XtraReport1.cs
@@ -0,0 +1,14 @@
+﻿using DevExpress.XtraReports.UI;
+using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Drawing;
+
+namespace WindowsFormsApp4 {
+    public partial class XtraReport1 : DevExpress.XtraReports.UI.XtraReport {
+        public XtraReport1() {
+            InitializeComponent();
+        }
+
+    }
+}
